a1b18bc016cd967dcd684148cd93290ecf2a3b3b
M sandoval <sandoval.mario.08@gmail.com>
2025-05-05 01:20:24 -0400

    feat: add const of menu

---
+++ src/consts/menu.ts
@@ -0,0 +1,20 @@
+import type { Menu } from "@types/menu";
+
+export const MENU: Menu[] = [
+  {
+    title: "Home",
+    url: "/"
+  },
+  {
+    title: "Servicios",
+    url: "#services"
+  },
+  {
+    title: "Proyectos",
+    url: "#project"
+  },
+  {
+    title: "Nosotros",
+    url: "#about"
+  }
+]
