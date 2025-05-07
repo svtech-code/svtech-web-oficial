6057c4a537d3eff124e49a4c66662e06ce7544fe
M sandoval <sandoval.mario.08@gmail.com>
2025-05-07 01:15:59 -0400

    build: add dependences

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   output: "static",
+  adapter: vercel(),
   // trailingSlash: "always",
-  site: "https://www.svtech.cl",
+  // site: "https://www.svtech.cl",
 });
