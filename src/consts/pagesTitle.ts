2c5f0bd8a05f1b39a21a6bc39f6a75d72ba84cde
M sandoval <sandoval.mario.08@gmail.com>
2025-05-10 16:59:29 -0400

    feat: add news titles

--- src/consts/pagesTitle.ts
+++ src/consts/pagesTitle.ts
@@ -1,2 +1,12 @@
-export const fixedTitle: string = "Sv Tech";
+export const fixedTitle: string = 'Sv Tech';
 
+// servicios
+export const titleSerive: string = 'Nuestros Servicios';
+export const subTitleService: string =
+  'Brindamos servicios de calidad, asegurando la continuidad operativa de equipos tecnológicos y sistemas informáticos';
+
+// por que elegirnos
+export const smallTitleWhy: string = 'Por que elegirnos';
+export const titleWhy: string = 'Experiencia y calidad a tu sercicio';
+export const subTitleWhy: string =
+  'Aumenta el rendimiento de tus equipos y mejora la productividad de tu negocio, manteniendo sus equipos tecnológicos y sistemas web en óptimas condiciones.';
