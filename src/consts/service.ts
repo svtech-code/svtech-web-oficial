83ca52f6be68e2239d498f1b9ea9548671d1940d
M sandoval <sandoval.mario.08@gmail.com>
2025-05-10 16:59:44 -0400

    feat: add services const

---
+++ src/consts/service.ts
@@ -0,0 +1,88 @@
+import type { Service } from 'types/service';
+
+export const SERVICE: Service[] = [
+  {
+    id: 'soporte-informatico',
+    title: 'Soporte informático',
+    description:
+      'Mantenimiento preventivo y correctivo de equipos informáticos, servidores y sistemas operativos.',
+    icon: {
+      svg: 'computer',
+      width: 0,
+      height: 0,
+    },
+    features: ['Mantenimiento preventivo y correctivo'],
+    featured: false,
+    content: '',
+  },
+  {
+    id: 'desarrollo-web',
+    title: 'Desarrollor web',
+    description:
+      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+    icon: {
+      svg: 'computer',
+      width: 0,
+      height: 0,
+    },
+    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    featured: false,
+    content: '',
+  },
+  {
+    id: 'desarrollo-web',
+    title: 'Desarrollor web',
+    description:
+      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+    icon: {
+      svg: 'computer',
+      width: 0,
+      height: 0,
+    },
+    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    featured: false,
+    content: '',
+  },
+  {
+    id: 'desarrollo-web',
+    title: 'Desarrollor web',
+    description:
+      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+    icon: {
+      svg: 'computer',
+      width: 0,
+      height: 0,
+    },
+    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    featured: false,
+    content: '',
+  },
+  {
+    id: 'desarrollo-web',
+    title: 'Desarrollor web',
+    description:
+      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+    icon: {
+      svg: 'computer',
+      width: 0,
+      height: 0,
+    },
+    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    featured: false,
+    content: '',
+  },
+  {
+    id: 'desarrollo-web',
+    title: 'Desarrollor web',
+    description:
+      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+    icon: {
+      svg: 'computer',
+      width: 0,
+      height: 0,
+    },
+    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    featured: false,
+    content: '',
+  },
+];
