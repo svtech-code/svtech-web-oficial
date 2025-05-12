91ab11e387a6f0af779556229d2a8364d6bb251e
M sandoval <sandoval.mario.08@gmail.com>
2025-05-12 00:52:26 -0400

    feat: add icons and descriptions of services

--- src/consts/service.ts
+++ src/consts/service.ts
@@ -1,5 +1,12 @@
 import type { Service } from 'types/service';
 
+import SoporteInformatico from "assets/services/soporte-informatico.svg";
+import DesarrolloWeb from "assets/services/desarrollo-web.svg";
+import ConsultoriaTi from "assets/services/consultoria-ti.svg";
+import AdministracionHosting from "assets/services/administracion-hosting.svg";
+import BaseDatos from "assets/services/base-datos.svg";
+import SoportePersonalizado from "assets/services/soporte-personalizado.svg";
+
 export const SERVICE: Service[] = [
   {
     id: 'soporte-informatico',
@@ -7,9 +14,9 @@ export const SERVICE: Service[] = [
     description:
       'Mantenimiento preventivo y correctivo de equipos informáticos, servidores y sistemas operativos.',
     icon: {
-      svg: 'computer',
-      width: 0,
-      height: 0,
+      svg: SoporteInformatico,
+      width: 50,
+      height: 50,
     },
     features: ['Mantenimiento preventivo y correctivo'],
     featured: false,
@@ -21,67 +28,67 @@ export const SERVICE: Service[] = [
     description:
       'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
     icon: {
-      svg: 'computer',
-      width: 0,
-      height: 0,
+      svg: DesarrolloWeb,
+      width: 50,
+      height: 50,
     },
     features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
     featured: false,
     content: '',
   },
   {
-    id: 'desarrollo-web',
-    title: 'Desarrollor web',
+    id: 'consultoria-ti',
+    title: 'Consultoría TI',
     description:
-      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+      'Asesoramiento tecnológico para mejorar y optimizar procesos y recursos de tu empresa o emprendimiento.',
     icon: {
-      svg: 'computer',
-      width: 0,
-      height: 0,
+      svg: ConsultoriaTi,
+      width: 50,
+      height: 50,
     },
-    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    features: ['Sistemas', 'Equipos', 'Seguridad'],
     featured: false,
     content: '',
   },
   {
-    id: 'desarrollo-web',
-    title: 'Desarrollor web',
+    id: 'administracion-hosting',
+    title: 'Administración hosting',
     description:
-      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+      'Administración, configuración mantenimiento o migración de servicio de alojamiento web.',
     icon: {
-      svg: 'computer',
-      width: 0,
-      height: 0,
+      svg: AdministracionHosting,
+      width: 50,
+      height: 50,
     },
-    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    features: [],
     featured: false,
     content: '',
   },
   {
-    id: 'desarrollo-web',
-    title: 'Desarrollor web',
+    id: 'base-datos',
+    title: 'Base datos',
     description:
-      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+      'Diseño, implementación y mantenimiento de bases de datos relacionales, para cualquier tipo de negocio.',
     icon: {
-      svg: 'computer',
-      width: 0,
-      height: 0,
+      svg: BaseDatos,
+      width: 50,
+      height: 50,
     },
-    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    features: ["seguridad", "disponibilidad", "integridad"],
     featured: false,
     content: '',
   },
   {
-    id: 'desarrollo-web',
-    title: 'Desarrollor web',
+    id: 'soporte-personalizado',
+    title: 'Soporte personalizado',
     description:
-      'Creación de sistemas web y landing page modernas, flexibles, escalables y responsivas.',
+      'Asesoramiento tecnológico para mejorar y optimizar procesos y recursos de tu empresa o emprendimiento.',
     icon: {
-      svg: 'computer',
-      width: 0,
-      height: 0,
+      svg: SoportePersonalizado,
+      width: 50,
+      height: 50,
     },
-    features: ['Diseño UX/UI', 'aplicaciones web', 'responsive designe'],
+    features: ["Continuidad", "Responsabilidad", "Acompañamiento"],
     featured: false,
     content: '',
   },
