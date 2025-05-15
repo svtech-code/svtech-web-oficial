efc2cd740338e52759d01a0177fc44b6ed1f8be9
M sandoval <sandoval.mario.08@gmail.com>
2025-05-14 21:33:08 -0400

    chore: add must titles

--- src/consts/pagesTitle.ts
+++ src/consts/pagesTitle.ts
@@ -6,7 +6,12 @@ export const subTitleService: string =
   'Brindamos servicios de calidad, asegurando la continuidad operativa de equipos tecnológicos y sistemas informáticos';
 
 // por que elegirnos
-export const smallTitleWhy: string = 'Por que elegirnos';
+export const smallTitleWhy: string = '¿Por que elegirnos?';
 export const titleWhy: string = 'Experiencia y calidad a tu sercicio';
-export const subTitleWhy: string =
+export const textWhy: string =
   'Aumenta el rendimiento de tus equipos y mejora la productividad de tu negocio, manteniendo sus equipos tecnológicos y sistemas web en óptimas condiciones.';
+
+
+// proyectos
+export const titleProject: string = "Proyectos Realizados"
+export const subTitleProject: string = "Conoce algunos de los trabajos más destacados, que han sido desarrollados e implementados en pequeñas y medianas empresas"
