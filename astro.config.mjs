c80b7e3f6e54b3c6522fd88863cd3aa2d9b02c11
M sandoval <sandoval.mario.08@gmail.com>
2025-05-25 12:18:59 -0400

    build: delete site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   output: 'server',
   adapter: vercel(),
-  site: 'https://www.svtech.cl/',
 });
