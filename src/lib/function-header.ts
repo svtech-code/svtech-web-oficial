78e653a6ad8988c32b4dbd6c22f7d7b4f95a2e76
M sandoval <sandoval.mario.08@gmail.com>
2025-05-27 01:21:46 -0400

    feat: add close toggle in navbar mobile

--- src/lib/function-header.ts
+++ src/lib/function-header.ts
@@ -1,16 +1,24 @@
-
 export const functionHeader = () => {
-  const target = document.querySelector<HTMLDivElement>('[data-target]')
-  const navbarButton = document.querySelector<HTMLElement>('#navbar-button')
-  const contentNavbarDesktop = document.querySelector<HTMLDivElement>("#content-navbar-desktop")
-  const contentNavbarMobile = document.querySelector<HTMLDivElement>("#content-navbar-mobile")
-  const navbarMobile = document.querySelector<HTMLElement>("#navbar-mobile")
+  const target = document.querySelector<HTMLDivElement>('[data-target]');
+  const navbarButton = document.querySelector<HTMLElement>('#navbar-button');
+  const contentNavbarDesktop = document.querySelector<HTMLDivElement>('#content-navbar-desktop');
+  const contentNavbarMobile = document.querySelector<HTMLDivElement>('#content-navbar-mobile');
+  const navbarMobile = document.querySelector<HTMLElement>('#navbar-mobile');
+  const navbarMobileLinks = navbarMobile?.querySelectorAll('ul li a');
 
   const handleIntersection = ([entry]: IntersectionObserverEntry[]) => {
     if (!entry.isIntersecting) {
-      contentNavbarDesktop?.classList.remove('md:backdrop-blur-none', 'md:bg-transparent', 'md:shadow-none');
+      contentNavbarDesktop?.classList.remove(
+        'md:backdrop-blur-none',
+        'md:bg-transparent',
+        'md:shadow-none',
+      );
     } else {
-      contentNavbarDesktop?.classList.add('md:backdrop-blur-none', 'md:bg-transparent', 'md:shadow-none');
+      contentNavbarDesktop?.classList.add(
+        'md:backdrop-blur-none',
+        'md:bg-transparent',
+        'md:shadow-none',
+      );
     }
   };
 
@@ -22,14 +30,23 @@ export const functionHeader = () => {
 
   const toggleMenu = () => {
     navbarButton?.classList.toggle('open');
-    contentNavbarMobile?.classList.toggle("w-full")
-    const isOpenMenuMobil = navbarMobile?.classList.toggle("w-[60%]")
+    contentNavbarMobile?.classList.toggle('w-full');
+    const isOpenMenuMobil = navbarMobile?.classList.toggle('w-[60%]');
 
-    navbarMobile?.setAttribute("aria-hidden", `${isOpenMenuMobil}`)
-    navbarMobile?.setAttribute("aria-expanded", `${isOpenMenuMobil}`)
-  }
+    navbarMobile?.setAttribute('aria-hidden', `${isOpenMenuMobil}`);
+    navbarMobile?.setAttribute('aria-expanded', `${isOpenMenuMobil}`);
+  };
+
+  const toggleMenuLinks = () => {
+    navbarMobileLinks?.forEach((link) => {
+      link.addEventListener('click', () => {
+        toggleMenu();
+      });
+    });
+  };
 
   if (target) createObserver(target, handleIntersection);
 
-  navbarButton?.addEventListener("click", toggleMenu)
-}
+  navbarButton?.addEventListener('click', toggleMenu);
+  toggleMenuLinks();
+};
