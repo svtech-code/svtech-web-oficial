f51891372769cdfe598a18e46a2553e56eed7f9c
M sandoval <sandoval.mario.08@gmail.com>
2025-06-08 15:55:02 -0400

    build: dependence for sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: 'server',
   adapter: vercel(),
+  integrations: [sitemap()],
+  site: 'https://www.svtech.cl',
 });
+
