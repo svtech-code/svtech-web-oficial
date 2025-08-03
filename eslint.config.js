5e9de2aa943a1e6abe902a1025bd3bec91aed086
M sandoval <sandoval.mario.08@gmail.com>
2025-08-03 19:41:02 -0400

    build: update tsconfig and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,28 @@
-import eslintPluginAstro from 'eslint-plugin-astro'
-import { defineConfig } from 'eslint/config'
+import eslintPluginAstro from 'eslint-plugin-astro';
+import { defineConfig } from 'eslint/config';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default defineConfig([
   ...eslintPluginAstro.configs.recommended,
   {
+    files: ['**/*.{ts}'],
+    plugins: { '@typescript-eslint': tsPlugin },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
     rules: {
       'comma-dangle': ['error', 'always-multiline'],
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
-])
+
+  //ignora archivos específicos
+  {
+    ignores: ['dist/**', 'node_modules/**', '.astro/**'],
+  },
+]);
