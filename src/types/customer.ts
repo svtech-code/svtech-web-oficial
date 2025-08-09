74db3d9cb6794b66cb02e95873cfe38248c4ee48
M sandoval <sandoval.mario.08@gmail.com>
2025-08-09 00:04:33 -0400

    fix: update property types

--- src/types/customer.ts
+++ src/types/customer.ts
@@ -1,10 +1,22 @@
+export interface SocialMedia {
+  instagram?: string;
+  facebook?: string;
+  linkedin?: string;
+  twitter?: string;
+  website?: string;
+}
+
 export interface Customer {
   id: string;
   name: string;
   shortName: string;
   description: string;
-  image: {
+  fullDescription: string;
+  logo: {
     svg: any;
   };
   url: string;
+  socialMedia: SocialMedia;
+  industry: string;
+  location: string;
 }
