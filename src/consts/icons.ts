1ed6e0a7098ac9f7392d2323084010ef31474aa1
M sandoval <sandoval.mario.08@gmail.com>
2025-08-10 00:35:14 -0400

    fix: update of use icons and imports

--- src/consts/icons.ts
+++ src/consts/icons.ts
@@ -7,6 +7,11 @@ import Facebook from 'assets/svg/facebook.svg';
 
 // servicios
 import SoporteInformatico from 'assets/services/soporte-informatico.svg';
+import DesarrolloWeb from 'assets/services/desarrollo-web.svg';
+import ConsultoriaTi from 'assets/services/consultoria-ti.svg';
+import AdministracionHosting from 'assets/services/administracion-hosting.svg';
+import BaseDatos from 'assets/services/base-datos.svg';
+import SoportePersonalizado from 'assets/services/soporte-personalizado.svg';
 
 export const icons = {
   // redes sociales
@@ -18,6 +23,11 @@ export const icons = {
 
   // servicios
   'soporte-informatico': SoporteInformatico,
+  'desarrollo-web': DesarrolloWeb,
+  'consultoria-ti': ConsultoriaTi,
+  'administracion-hosting': AdministracionHosting,
+  'base-datos': BaseDatos,
+  'soporte-personalizado': SoportePersonalizado,
 } as const;
 
 export type IconName = keyof typeof icons;
