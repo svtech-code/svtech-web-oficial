ff5c0ef6a1d4324af5e138d11b2ffde55bee5e1b
M sandoval <sandoval.mario.08@gmail.com>
2025-08-10 22:03:29 -0400

    feat: import of icons svg

--- src/consts/icons.ts
+++ src/consts/icons.ts
@@ -13,6 +13,19 @@ import AdministracionHosting from 'assets/services/administracion-hosting.svg';
 import BaseDatos from 'assets/services/base-datos.svg';
 import SoportePersonalizado from 'assets/services/soporte-personalizado.svg';
 
+// customers
+import Customer1 from 'assets/customers/customer-01.svg';
+import Customer2 from 'assets/customers/customer-02.svg';
+import Customer3 from 'assets/customers/customer-03.svg';
+import Customer4 from 'assets/customers/customer-04.svg';
+import Customer5 from 'assets/customers/customer-05.svg';
+import Customer6 from 'assets/customers/customer-06.svg';
+
+// others
+import IconCheck from 'assets/svg/check.svg';
+import Location from 'assets/svg/location.svg';
+import Chrome from 'assets/svg/chrome.svg';
+
 export const icons = {
   // redes sociales
   x: X,
@@ -20,6 +33,7 @@ export const icons = {
   discord: Discord,
   whatsApp: WhatsApp,
   facebook: Facebook,
+  website: Chrome,
 
   // servicios
   'soporte-informatico': SoporteInformatico,
@@ -28,6 +42,18 @@ export const icons = {
   'administracion-hosting': AdministracionHosting,
   'base-datos': BaseDatos,
   'soporte-personalizado': SoportePersonalizado,
+
+  // customers
+  'customer-01': Customer1,
+  'customer-02': Customer2,
+  'customer-03': Customer3,
+  'customer-04': Customer4,
+  'customer-05': Customer5,
+  'customer-06': Customer6,
+
+  // others
+  check: IconCheck,
+  location: Location,
 } as const;
 
 export type IconName = keyof typeof icons;
