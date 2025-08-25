4e645fb8016d0f51f6f2d1f111eab6c05adaa1f8
M sandoval <sandoval.mario.08@gmail.com>
2025-08-24 22:46:59 -0400

    fix: update name icon

--- src/types/customer.ts
+++ src/types/customer.ts
@@ -2,11 +2,15 @@ import type { IconName } from 'consts/icons';
 
 export interface SocialMedia {
   instagram?: string;
+  'instagram-color'?: string;
   facebook?: string;
+  'facebook-color'?: string;
   linkedin?: string;
   x?: string;
-  website?: string;
+  chrome?: string;
+  'chrome-color'?: string;
   whatsApp?: string;
+  'whatsApp-color'?: string;
 }
 
 export interface Customer {
