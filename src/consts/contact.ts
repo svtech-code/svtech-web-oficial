dc09d062c0b13ac534adfc725bbfca0e58f62626
M sandoval <sandoval.mario.08@gmail.com>
2025-09-06 01:31:07 -0400

    feat: add data of contact

---
+++ src/consts/contact.ts
@@ -0,0 +1,37 @@
+import type { Contact } from 'types/contact';
+
+export const CONTACT_INFO: Contact[] = [
+  {
+    icon: 'phone',
+    label: 'Teléfono',
+    description: '+569 8793 3282',
+  },
+  {
+    icon: 'mail',
+    label: 'Correo',
+    description: 'contacto@svtech.cl',
+  },
+  {
+    icon: 'map',
+    label: 'Dirección',
+    description: 'Linares, Maule. Chile.',
+  },
+];
+
+export const CONTACT_SCHEDULE: Contact[] = [
+  {
+    icon: 'calendar',
+    label: 'Atención',
+    description: 'Lunes a Sábado',
+  },
+  {
+    icon: 'clock',
+    label: 'Mañana',
+    description: '09:00 - 14:00 hrs.',
+  },
+  {
+    icon: 'clock',
+    label: 'Tarde',
+    description: '15:00 - 20:00 hrs.',
+  },
+];
