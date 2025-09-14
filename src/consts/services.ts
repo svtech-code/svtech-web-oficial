90d3c7ebe22bf2d82f397c2a34a1600266a7bec9
M sandoval <sandoval.mario.08@gmail.com>
2025-09-14 15:59:11 -0300

    feat: add object of service by card services

---
+++ src/consts/services.ts
@@ -0,0 +1,49 @@
+import type { ServiceStatic } from 'types/services';
+
+export const SERVICES_STATIC: ServiceStatic[] = [
+  {
+    order: 1,
+    slug: 'soporte-informatico',
+    title: 'Soporte Informático',
+    description:
+      'Soporte técnico especializado para mantener tus sistemas funcionando de manera óptima.',
+    iconName: 'soporte-informatico',
+  },
+  {
+    order: 2,
+    slug: 'desarrollo-web',
+    title: 'Desarrollo web',
+    description:
+      'Desarrollo de aplicaciones web y landing page modernas, flexibles, escalables y responsivas.',
+    iconName: 'desarrollo-web',
+  },
+  {
+    order: 3,
+    slug: 'consultoria-ti',
+    title: 'Consultoría TI',
+    description: 'Asesoramiento tecnológico estratégico para optimizar tus procesos empresariales.',
+    iconName: 'consultoria-ti',
+  },
+  {
+    order: 4,
+    slug: 'administracion-hosting',
+    title: 'Administración Hosting',
+    description: 'Gestión completa de servidores y servicios de hosting para máximo rendimiento.',
+    iconName: 'administracion-hosting',
+  },
+  {
+    order: 5,
+    slug: 'base-datos',
+    title: 'Base de Datos',
+    description: 'Diseño, implementación y mantenimiento de bases de datos eficientes y seguras.',
+    iconName: 'base-datos',
+  },
+  {
+    order: 6,
+    slug: 'soporte-personalizado',
+    title: 'Soporte Personalizado',
+    description:
+      'Atención técnica especializada adaptada a las necesidades específicas de tu proyecto.',
+    iconName: 'soporte-personalizado',
+  },
+];
