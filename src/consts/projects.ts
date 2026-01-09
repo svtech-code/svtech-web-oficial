402897bd7d3363ef68125746caa1f3fba2944f80
M Sandoval <msandoval@Ms-MacBook-Pro.local>
2026-01-08 23:27:47 -0300

    feat: add section project and consts from project

---
+++ src/consts/projects.ts
@@ -0,0 +1,32 @@
+export const PROJECTS_STATIC = [
+  {
+    order: 1,
+    type: 'desarrollo',
+    title: 'sistema alerta temprana',
+    description:
+      'Sistema de alerta temprana, para la identificacion de estudiantes con riesgo de descerción.',
+    imageSrc: '/images/services/administracion-hosting.webp',
+    imageAlt: 'Imagen del sistema de alerta temprana',
+    customer: 'CFT del Maule',
+  },
+  {
+    order: 2,
+    type: 'servicio',
+    title: 'migracion servicio hosting',
+    description:
+      'Migración y configuración de los servicios de hosting, que incluyen; Página web y correos corporativos.',
+    imageSrc: '/images/services/administracion-hosting.webp',
+    imageAlt: 'Imagen relacionada la misgracion de hosting',
+    customer: 'Constructora BYA Ltda.',
+  },
+  {
+    order: 3,
+    type: 'desarrollo',
+    title: 'Plantilla con Apps Script',
+    description:
+      'Plantilla programada con apps script y funciones avanzadas, para el registro y control de servicios de telemedicina.',
+    imageSrc: '/images/services/administracion-hosting.webp',
+    imageAlt: 'Capturas de las diferentes vistas de la plantilla',
+    customer: 'SAMED',
+  },
+];
