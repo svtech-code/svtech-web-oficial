c17ce9f1239ba431f582fec03c78893732469a1f
M Sandoval <msandoval@Ms-MacBook-Pro.local>
2026-01-09 23:21:30 -0300

    fix: update information of projects

--- src/consts/projects.ts
+++ src/consts/projects.ts
@@ -1,32 +1,37 @@
+const path = (name: string) => {
+  const pathProject = '/images/projects/';
+  return `${pathProject}${name}.webp`;
+};
+
 export const PROJECTS_STATIC = [
   {
     order: 1,
     type: 'desarrollo',
-    title: 'sistema alerta temprana',
+    title: 'sistema para pre-matrícula',
     description:
-      'Sistema de alerta temprana, para la identificacion de estudiantes con riesgo de descerción.',
-    imageSrc: '/images/services/administracion-hosting.webp',
-    imageAlt: 'Imagen del sistema de alerta temprana',
-    customer: 'CFT del Maule',
+      'Sistema para proceso de pre-matrícula de un establecimiento educacional de Linares.',
+    imageSrc: path('sistema-prematricula'),
+    imageAlt: 'Imagen que presenta en diferentes dispositivos, la página web de pre-matrícula',
+    customer: 'Liceo Valentín Letelier Madariaga',
   },
   {
     order: 2,
+    type: 'desarrollo',
+    title: 'sistema alerta temprana',
+    description:
+      'Sistema de alerta temprana, para la identificacion de estudiantes con riesgo de descerción.',
+    imageSrc: path('sistema-alerta-temprana'),
+    imageAlt: 'Imagen que presenta en diferentes dispositivos, el sistema de alerta temprana',
+    customer: 'CFT del Maule',
+  },
+  {
+    order: 3,
     type: 'servicio',
     title: 'migracion servicio hosting',
     description:
       'Migración y configuración de los servicios de hosting, que incluyen; Página web y correos corporativos.',
-    imageSrc: '/images/services/administracion-hosting.webp',
-    imageAlt: 'Imagen relacionada la misgracion de hosting',
+    imageSrc: path('migracion-hosting'),
+    imageAlt: 'Imagen relacionada con el proceso de migración se servicios de hosting',
     customer: 'Constructora BYA Ltda.',
   },
-  {
-    order: 3,
-    type: 'desarrollo',
-    title: 'Plantilla con Apps Script',
-    description:
-      'Plantilla programada con apps script y funciones avanzadas, para el registro y control de servicios de telemedicina.',
-    imageSrc: '/images/services/administracion-hosting.webp',
-    imageAlt: 'Capturas de las diferentes vistas de la plantilla',
-    customer: 'SAMED',
-  },
-];
+] as const;
